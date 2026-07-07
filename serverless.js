e3018cc25c8edb41f12e27dec8c6294d8cd21e19
A-str0 <il.eroxin@gmail.com>
2026-07-08 01:27:43 +0300

    fix: add error handling to serverless.js, remove includeFiles

--- serverless.js
+++ serverless.js
@@ -1,5 +1,23 @@
 const { getRouter } = require("stremio-addon-sdk");
-const addonInterface = require("./dist/addon");
+
+let addonInterface;
+try {
+  addonInterface = require("./dist/addon").addonInterface;
+} catch (e) {
+  module.exports = function (_req, res) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ err: "addon load error", msg: e.message }));
+  };
+  return;
+}
+
+if (!addonInterface) {
+  module.exports = function (_req, res) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ err: "addonInterface is undefined" }));
+  };
+  return;
+}
 
 const router = getRouter(addonInterface);
 
