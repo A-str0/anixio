9cbc68c8b81f5b2363020ce854b168c576d75ff5
A-str0 <il.eroxin@gmail.com>
2026-07-08 01:28:37 +0300

    chore: clean up serverless.js, remove debug error handling

--- serverless.js
+++ serverless.js
@@ -1,23 +1,5 @@
 const { getRouter } = require("stremio-addon-sdk");
-
-let addonInterface;
-try {
-  addonInterface = require("./dist/addon").addonInterface;
-} catch (e) {
-  module.exports = function (_req, res) {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ err: "addon load error", msg: e.message }));
-  };
-  return;
-}
-
-if (!addonInterface) {
-  module.exports = function (_req, res) {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ err: "addonInterface is undefined" }));
-  };
-  return;
-}
+const { addonInterface } = require("./dist/addon");
 
 const router = getRouter(addonInterface);
 
